8e7bb5484a0b41c705f6125b143126d67c65f876
AnFois <an.fois@reply.it>
2024-01-19 15:31:18 +0100

    fix

--- template/scripts/main.js
+++ template/scripts/main.js
@@ -3,12 +3,13 @@ function executeWidgetCode() {
     require( ["scripts/vue.min", "DS/DataDragAndDrop/DataDragAndDrop"],   function(Vue, DataDragAndDrop) {
 
         Vue.component('my-component', {
+            props:['fullData'],
             template:`
             <div id="dropObj" style="border: dashed #22BC3C; background-color: #F0FFF3;
                 color: black;text-align: center; display: flex; flex-direction: column; 
                 justify-content: center; align-items: center; font-size: 18px;">
                 <div v-for="(r, index) in fullData" v-bind="r">
-                <b>{{r.title}}</b></div>
+                <b>{{r}}</b></div>
                 <div>
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-file-earmark-check" viewBox="0 0 16 16">
                 <path d="M10.854 7.854a.5.5 0 0 0-.708-.708L7.5 9.793 6.354 8.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
@@ -37,46 +38,35 @@ function executeWidgetCode() {
                 },
 
                 makeDroppable: function () {
-                    var allElems = widget.body.querySelectorAll(".dragEligible");
-                    for (var i = 0; i < allElems.length; i++) {
-                        DataDragAndDrop.droppable(allElems[i], {
-                            drop: function (data) {
-                                this.fullData = JSON.parse(data).data.items.map(
-                                    (e) => {
-                                        this.callData();
-                                        if (e.objectType === "VPMReference") {
-                                            if (this.dataAttrEng.length) {
-                                                this.dataAttrEng.length = 0
-                                            }
-                                            this.dataEng(e.objectId);
-                                        }
-                                        if (this.dataClassAttr) {
-                                            this.dataClassAttr.length = 0;
-                                        }
-                                        this.classAttr(e.objectId);
-                                        return {
-                                            title: e.displayName,
-                                            type: e.objectType,
-                                            id: e.objectId,
-                                            revision: e.version
-                                        }
-                                    })
-                                var myButton = document.getElementById("generateEIN");
-                                myButton.disabled = false;
-    
-                            }
-                        });
-                        this.displayData = true;
-                        this.makeDroppable();
-                    }
+                    DataDragAndDrop.droppable(dropElement, {
+                        drop: function(data){
+                            var arrayData=[];
+                            arrayData.push(data);
+                            this.fullData = arrayData;
+                            this.displayData = true;
+                            widget.body.style="border:5px hidden;"
+                        },
+                        enter: function(){
+                            console.log("Enter");
+                            widget.body.style="border:5px solid orange;"
+                        },
+                        leave: function(){
+                            console.log("Leave");
+                            widget.body.style="border:5px solid red;"
+                            
+                        },
+                        over: function(){
+                            console.log("Over");
+                            widget.body.style="border:5px solid orange;"
+                        } 
+                        
+                    })
                 },
             }
         });
 
         widget.addEvent('onLoad', function(){
-            widget.addEvent('onRefresh', function(preferences){
-                myWidgetApp.makeDroppable();
-            })
+            myWidgetApp.makeDroppable();
         })
     });
         
