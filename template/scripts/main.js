2216a7e483fd69d710926257669d7beafe34b255
AnFois <an.fois@reply.it>
2024-01-19 09:27:49 +0100

    mounting app in main.js

--- template/scripts/main.js
+++ template/scripts/main.js
@@ -3,6 +3,21 @@ function executeWidgetCode() {
   require(["UWA/Drivers/jQuery"], function($) {
       var myWidget = {
           onLoadWidget: function(){
+            const app = Vue.createApp({
+                data() {
+                    return {
+                        show: true,
+                        message: "Show"
+                    };
+                },
+                methods: {
+                    toggleShow() {
+                        this.show = !this.show;
+                    }
+                }
+            });
+
+            app.mount('#app');
           },
       };
 
