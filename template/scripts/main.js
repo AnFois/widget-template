93589de861d1c0778116af144fa9f60f4ffbbdc6
AnFois <an.fois@reply.it>
2024-01-30 12:04:47 +0100

    minor fixes

--- template/scripts/main.js
+++ template/scripts/main.js
@@ -23,6 +23,7 @@ function executeWidgetCode() {
             data: {
                show: true,
                fullData: [],
+               displayDaya : false,
             },
 
             methods: {
@@ -40,17 +41,22 @@ function executeWidgetCode() {
                     DataDragAndDrop.droppable(dropElement, {
                         drop: function(data){
                             console.log("Drop");
+                            this.displayDaya = true;
+                            borderDiv.style="text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; border: solid #118921; color: #118921"
                         },
                         enter: function(){
                             console.log("Enter");
+                            this.displayDaya = false;
                         },
                         leave: function(){
                             console.log("Leave");
+                            this.displayDaya = false;
                             borderDiv.style="text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; border: solid red;"                            
                             
                         },
                         over: function(){
                             console.log("Over");
+                            this.displayDaya = false;
                             borderDiv.style="text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; border: solid orange;"
                         } 
                         
