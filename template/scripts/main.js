ee721366a94f7dd700ed40493a1688dff2b3eb7e
AnFois <an.fois@reply.it>
2024-02-08 16:10:24 +0100

    v-if try

--- template/scripts/main.js
+++ template/scripts/main.js
@@ -3,18 +3,18 @@ function executeWidgetCode() {
     require( ["scripts/vue.min", "DS/DataDragAndDrop/DataDragAndDrop"],   function(Vue, DataDragAndDrop) {
 
         Vue.component('my-component', {
-            props:['displayData'],
+            props:['fullData'],
             template:`
-            <div id="borderDiv" style="text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center;
-				border: dashed #2B3669; color: #2B3669;
-				font-size: 14px;">
-				<div v-if="!displayData">Drop here object to code</div>
-				<div v-if="displayData">Object dropped </div>
-				<div v-if="!displayData"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
-					<path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
-					<path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
-				</svg></div>
-			</div>`
+            <div id="dropObj" style="border: dashed #22BC3C; background-color: #F0FFF3;
+                color: black;text-align: center; display: flex; flex-direction: column; 
+                justify-content: center; align-items: center; font-size: 18px;">
+                <div v-for="(r, index) in fullData" v-bind="r">
+                <b>{{r}}</b></div>
+                <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-file-earmark-check" viewBox="0 0 16 16">
+                <path d="M10.854 7.854a.5.5 0 0 0-.708-.708L7.5 9.793 6.354 8.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
+                <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
+                </svg></div></div>`
         });
 
         let myWidgetApp = new Vue({
@@ -28,7 +28,7 @@ function executeWidgetCode() {
 
             methods: {
                 toggleShow: function() {
-                    this.displayData = !this.displayData;
+                    this.show = !this.show;
                 },
 
                 navigateTo: async function(page){
@@ -41,24 +41,20 @@ function executeWidgetCode() {
                     DataDragAndDrop.droppable(dropElement, {
                         drop: function(data){
                             borderDiv.style="text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; border: solid #118921; color: #118921";        
-                            //this.displayData = !this.displayData;
-                            this.toggleShow();
+                            this.displayData = !this.displayData;
                             myWidgetApp.makeDroppable();
                         },
                         enter: function(){
                             console.log("Enter");
-                            //this.displayData = false;
-                            this.toggleShow();
+                            this.displayData = false;
                         },
                         leave: function(){
                             borderDiv.style="text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; border: solid red;";                                                        
-                            //this.displayData = false;
-                            this.toggleShow();
+                            this.displayData = false;
                         },
                         over: function(){
                             borderDiv.style="text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; border: solid orange;";                            
-                            //this.displayData = false;
-                            this.toggleShow();
+                            this.displayData = false;
                         } 
                         
                     })
